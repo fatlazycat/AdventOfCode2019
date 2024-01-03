96b13783b06651bc29ea8173c67ef28b332b8289
Graham Berks <graham@fatlazycat.com>
2024-01-03 09:57:12 +0000

    Add shortest_path function and improve create_line_string in day_3.py
    A new functionality has been added in day_3.py with the introduction of 'shortest_path' function. The 'create_line_string' function has been updated to initialize 'line_string' with the point (0, 0). New modules 'length' and 'split' from 'shapely' have also been imported for computation in the new function. The test data has been placed directly within the code for verification purposes.

--- source/day_3.py
+++ source/day_3.py
@@ -1,7 +1,10 @@
 from source.utils import parse_file
 from enum import Enum
 from shapely.geometry import Point, LineString
+from shapely.measurement import length as line_length
+from shapely.ops import split
 from shapely import intersection_all
+from typing import List
 
 
 class Direction(Enum):
@@ -21,7 +24,7 @@ def process_data(rows):
 
 
 def create_line_string(data):
-    line_string = []
+    line_string = [(0, 0)]
     current_point = (0, 0)
     for direction, distance in data:
         if direction == Direction.UP:
@@ -47,6 +50,30 @@ def min_distance(data):
     list_of_points = list(intersections.geoms)
     distances = list(map(manhattan_distance, list_of_points))
     return min(distances)
+
+
+def shortest_path(data: List[str]):
+    line_strings = list(map(create_line_string, data))
+    lines = list(map(LineString, line_strings))
+    intersections = intersection_all(lines)
+    list_of_intersections = list(intersections.geoms)
+    distances = []
+
+    for intersection in list_of_intersections:
+        line_distances = 0
+        for line in lines:
+            line_parts = split(line, intersection)
+            line_distances += line_length(line_parts.geoms[0])
+        distances.append(line_distances)
+    return min(distances)
+
+
+test_data = ["R75,D30,R83,U83,L12,D49,R71,U7,L72",
+             "U62,R66,U55,R34,D71,R55,D58,R83"]
+# test_data = ["R8,U5,L5,D3",
+#              "U7,R6,D4,L4"]
+data = process_data(test_data)
+print(shortest_path(data))
 
 
 __all__ = ['parse_file', 'process_data', 'min_distance']
